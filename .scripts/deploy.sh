d1341e7c02f81547d0a46438ea1936fb00f13f0b
Md. Ahata Sham Ul Haque Tushar <mtushar53@gmail.com>
2025-05-03 21:40:04 +0600

    Update deploy.sh

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Load nvm (adjust the path if needed)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Use Node.js 18
+nvm use v22.14.0
+
 echo "Deployment started..."
 
 # Pull the latest version of the app
